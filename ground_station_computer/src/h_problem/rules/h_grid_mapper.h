d6cd4049c22f3221b3410a90e5e2a4896818a97b
Wanqiq7 <Wanqiq7@users.noreply.github.com>
2026-07-17 16:32:10 +0800

    fix: validate H grid cell identifiers

--- ground_station_computer/src/h_problem/rules/h_grid_mapper.h
+++ ground_station_computer/src/h_problem/rules/h_grid_mapper.h
@@ -3,8 +3,11 @@
 #include <QPoint>
 #include <QString>
 
+#include <optional>
+
 class GridMapper {
 public:
+    static std::optional<QPoint> tryToPoint(const QString &code);
     static QPoint toPoint(const QString &code);
     static QString toCode(const QPoint &point);
 };
